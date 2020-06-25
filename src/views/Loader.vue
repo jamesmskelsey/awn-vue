0a4fd9bad1d57e539969888dade8159fdc32e665
James Kelsey <jamesmskelsey@gmail.com>
2020-06-24 19:48:27 -0700

    add file input to loader.vue

--- src/views/Loader.vue
+++ src/views/Loader.vue
@@ -9,7 +9,10 @@ export default {
 </style>
 
 <template>
-    <div>
-        I am the loader!
+    <div class="m-4">
+        <h1 class="text-2xl">Select your AWN .csv report</h1>
+        <p>Start by running an AWN report and exporting the results to .csv</p>
+        <p>Then, open that file with this tool.</p>
+        <input type="file" id="awnReport" v-on:change="handleAwnReport($data, $event)">
     </div>
 </template>
